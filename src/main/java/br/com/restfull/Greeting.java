f5895d28621a4ac33cda01abe7fc284f679b7773
gabriel-primesw <gabriel@primesw.com.br>
2023-12-11 23:29:21 -0300

    v. 0.0.1

---
+++ src/main/java/br/com/restfull/Greeting.java
@@ -0,0 +1,22 @@
+package br.com.restfull;
+
+public class Greeting {
+
+	private final Long id;
+	private final String content;
+
+	public Greeting(Long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
---
+++ src/main/java/br/com/restfull/GreetingController.java
@@ -0,0 +1,25 @@
+package br.com.restfull;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s";
+	private final AtomicLong id = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(
+			@RequestParam(value = "name", defaultValue = "World!") 
+			String name) {
+		Long targetId = id.incrementAndGet();
+		String targetName = String.format(template, name);
+		Greeting targetGreeting = new Greeting (targetId, targetName);
+		return targetGreeting;
+	}
+
+}
